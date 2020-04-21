a3bf1d2e18b4f58c641545daa6448f31fab8d246
Michael Haslauer <michael.haslauer@gmx.de>
2020-04-21 13:58:20 +0200

    firt working implementation, very dirty

--- src/components/main/rover/MarsRover.js
+++ src/components/main/rover/MarsRover.js
@@ -8,7 +8,7 @@ export class MarsRover {
     }
 
     execute(commands) {
-        this.executeAll(commands.split(""));
+        this.executeAll(commands.split("").reverse());
     }
 
     executeAll(commandsArray) {
@@ -20,7 +20,38 @@ export class MarsRover {
         }
     }
 
-    executeNext(nextCommand){
-
-    };
+    executeNext(nextCommand) {
+        if (nextCommand === 'L') {
+            if (this.direction === 'N') {
+                this.direction = 'W';
+            } else if (this.direction === 'S') {
+                this.direction = 'E';
+            } else if (this.direction === 'E') {
+                this.direction = 'N';
+            } else if (this.direction === 'W') {
+                this.direction = 'S';
+            }
+        } else if (nextCommand === 'R') {
+            if (this.direction === 'N') {
+                this.direction = 'E';
+            } else if (this.direction === 'S') {
+                this.direction = 'W';
+            } else if (this.direction === 'E') {
+                this.direction = 'S';
+            } else if (this.direction === 'W') {
+                this.direction = 'N';
+            }
+        } else if (nextCommand === 'M') {
+            let p = this.position;
+            if (this.direction === 'N') {
+                this.position = new Position(p.x - 1, p.y);
+            } else if (this.direction === 'S') {
+                this.position = new Position(p.x + 1, p.y);
+            } else if (this.direction === 'E') {
+                this.position = new Position(p.x, p.y + 1);
+            } else if (this.direction === 'W') {
+                this.position = new Position(p.x, p.y - 1);
+            }
+        }
+    }
 }
--- src/components/main/rover/MarsRover.spec.js
+++ src/components/main/rover/MarsRover.spec.js
@@ -2,10 +2,101 @@ import {MarsRover} from "./MarsRover";
 import {Position} from "../position";
 
 describe('The Mars Rover', () => {
+
     it(' has the correct default position and direction', () => {
-        const rover = new MarsRover();
+        let rover = new MarsRover();
         expect(rover.direction).toEqual('N');
-        expect(rover.position).toEqual(new Position(0,0))
+        expect(rover.position).toEqual(new Position(0, 0))
+    });
+
+    describe('when pointing north on position (3,3)', () => {
+        let rover = new MarsRover();
+        beforeEach(() => {
+            rover.direction = 'N';
+            rover.position = new Position(3,3);
+        });
+
+        it('handles the turn left command', () => {
+            rover.executeNext('L');
+            expect(rover.direction).toEqual('W');
+        });
+
+        it('handles the turn right command', () => {
+            rover.executeNext('R');
+            expect(rover.direction).toEqual('E');
+        });
+
+        it('handles the move command', () => {
+            rover.executeNext('M');
+            expect(rover.position).toEqual(new Position(2,3));
+        });
+    });
+
+    describe('when pointing south on position (3,3)', () => {
+        let rover = new MarsRover();
+        beforeEach(() => {
+            rover.direction = 'S';
+            rover.position = new Position(3, 3);
+        });
+        it('handles the turn left command', () => {
+            rover.executeNext('L');
+            expect(rover.direction).toEqual('E');
+        });
+
+        it('handles the turn right command', () => {
+            rover.executeNext('R');
+            expect(rover.direction).toEqual('W');
+        });
+
+        it('handles the move command', () => {
+            rover.executeNext('M');
+            expect(rover.position).toEqual(new Position(4,3));
+        });
+    });
+
+    describe('when pointing east on position (3,3)', () => {
+        let rover = new MarsRover();
+        beforeEach(() => {
+            rover.direction = 'E';
+            rover.position = new Position(3, 3);
+        });
+        it('handles the turn left command', () => {
+            rover.executeNext('L');
+            expect(rover.direction).toEqual('N');
+        });
+
+        it('handles the turn right command', () => {
+            rover.executeNext('R');
+            expect(rover.direction).toEqual('S');
+        });
+
+        it('handles the move command', () => {
+            rover.executeNext('M');
+            expect(rover.position).toEqual(new Position(3,4));
+        });
+    });
+
+    describe('when pointing west on position (3,3)', () => {
+        let rover = new MarsRover();
+        beforeEach(() => {
+            rover.direction = 'W';
+            rover.position = new Position(3, 3);
+        });
+        it('handles the turn left command', () => {
+            rover.executeNext('L');
+            expect(rover.direction).toEqual('S');
+        });
+
+        it('handles the turn right command', () => {
+            rover.executeNext('R');
+            expect(rover.direction).toEqual('N');
+        });
+
+        it('handles the move command', () => {
+            rover.executeNext('M');
+            expect(rover.position).toEqual(new Position(3,2));
+        });
     });
 });
+
 
